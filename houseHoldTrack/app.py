9e821afe571bba86daa56e71edd187f479166938
Coba0405 <vacky0405@gmail.com>
2025-07-09 14:13:07 +0900

    create graph

--- houseHoldTrack/app.py
+++ houseHoldTrack/app.py
@@ -1,53 +1,75 @@
 import requests
-import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
 
+appId_estat = '58d95338ccddec8cef46ef85c851e3cf4159dffa'
 url = "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
 
 params = {
-    "appId":'58d95338ccddec8cef46ef85c851e3cf4159dffa',
-    "statsDataId": "0003000797",
+    "appId": appId_estat,
     "lang": "J",
+    "statsDataId": "0003000799",
     "metaGetFlg": "Y",
     "cntGetFlg": "N",
+    "explanationGetFlg": "Y",
+    "annotationGetFlg": "Y",
     "sectionHeaderFlg": "1",
+    "replaceSpChars": "0",
     "cdArea": "00000",
+    "cdCat01": "019,058,205"
 }
 
 response = requests.get(url, params=params)
+jsonData = response.json()
+
+print(len(jsonData), jsonData.keys())
+print(len(jsonData['GET_STATS_DATA']), jsonData['GET_STATS_DATA'].keys())
+data_static = jsonData['GET_STATS_DATA']['STATISTICAL_DATA']
+print(len(data_static), data_static.keys())
 
 if response.status_code == 200:
     try:
         data = response.json()
-        values = data["GET_STATS_DATA"]["STATISTICAL_DATA"]["DATA_INF"]["VALUE"]
-
-        # print(data)
+        values = data_static["DATA_INF"]["VALUE"]
 
         cat01_dict = {}
-        for item in data["GET_STATS_DATA"]["STATISTICAL_DATA"]["CLASS_INF"]["CLASS_OBJ"]:
-            if item["@id"] == "cat01":
-                    for val in item["CLASS"]:
-                         cat01_dict[val["@code"]] = val["@name"]
+        for class_obj in data["GET_STATS_DATA"]["STATISTICAL_DATA"]["CLASS_INF"]["CLASS_OBJ"]:
+            if class_obj["@id"] == "cat01":
+                for item in class_obj["CLASS"]:
+                    cat01_dict[item["@code"]] =  item["@name"]
 
-        result_list = []
-        # 年度と金額だけを抽出
-        for value in values:
-            year = value["@time"][:4]
-            money = value["$"]
-            cat01 = value["@cat01"]
-            name = cat01_dict.get(cat01, "不明な項目")
+        data_list = []
+        for item in values:
+            year = item["@time"][:4]
+            item_code = item["@cat01"]
+            amount = float(item["$"])
+            item_name = cat01_dict.get(item_code, "不明な項目")
+            data_list.append([year, item_name, amount])
 
-            result_list.append({
-                "年": year,
-                "項目": name,
-                "金額": money
-            })
+        df = pd.DataFrame(data_list, columns=["年", "項目", "金額"])
+        df_grouped = df.groupby(['年', '項目'])['金額'].mean().reset_index()
+        # df["金額"] = df["金額"].astype(int)
+        # print(df.head(50))
 
-        with open("household_income.json", "w", encoding='utf-8') as file:
-            json.dump(result_list, file, indent=4, ensure_ascii=False)
+        for item_name in df_grouped["項目"].unique():
+            subset = df_grouped[df_grouped["項目"] == item_name]
+            plt.plot(subset["年"], subset["金額"], marker='o', label=item_name)
 
-        print("データをhousehold_income.jsonに保存しました")
+        # for item in df["項目"].unique():
+        #     subset = df[df["項目"] == item_name]
+        #     plt.plot(subset["年"], subset["金額"], label=item_name)
 
-    except json.JSONDecodeError as e:
-        print("JSONデコードエラー:", e)
+        plt.xlabel("年")
+        plt.ylabel("金額")
+        plt.title("収入・支出・預貯金の推移")
+        plt.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+
+    except Exception as e:
+        print("処理中にエラー:", e)
 else:
     print("リクエスト失敗", response.status_code)
